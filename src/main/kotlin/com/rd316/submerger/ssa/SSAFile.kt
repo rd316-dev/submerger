63aa528ac3edd9aa14de9595f59801ae979956f0
rd316 <rd316.gortor20@gmail.com>
2024-05-07 22:25:41 +0300

    remove a file

--- src/main/kotlin/com/rd316/submerger/ssa/SSAFile.kt
+++
@@ -1,9 +0,0 @@
-package com.rd316.submerger.ssa
-
-class SSAFile {
-    val scriptInfo  = ArrayList<Pair<String, String>>()
-    val styleFormat = ArrayList<String>()
-    val eventFormat = ArrayList<String>()
-    val styles      = ArrayList<SSAStyle>()
-    val events      = ArrayList<SSAEvent>()
-}
