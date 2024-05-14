25bd8b386a2040e1aabe076dd6b528b7d94126f6
Ruslan Abzalov <rd316.gortor20@gmail.com>
2024-05-14 18:33:45 +0300

    increase version to 1.0.1

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -33,7 +33,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "SubMerger"
-            packageVersion = "1.0.0"
+            packageVersion = "1.0.1"
         }
     }
 }
